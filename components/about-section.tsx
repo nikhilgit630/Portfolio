d71dc5ffad78d1d2b8af7ee1d53dc5c7af44868f
nikhilgit630 <nik16dube@gmail.com>
2025-11-06 23:09:28 +0530

    change test

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -42,7 +42,7 @@ const AboutSection = () => {
         {/* Left Column - Image */}
         <motion.div variants={itemVariants} className="relative">
           <div className="w-full aspect-square bg-gradient-to-br from-primary/20 to-accent/20 rounded-2xl overflow-hidden">
-            <img src={`${process.env.NEXT_PUBLIC_BASE_URL}/developer-avatar.jpg`} alt="Nikhil Dubey" className="w-full h-full object-cover" />
+            <img src="/developer-avatar.jpg"  alt="Nikhil Dubey" className="w-full h-full object-cover" />
           </div>
         </motion.div>
 
