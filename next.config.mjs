017f10f2f6419e209622d505086cddbdcb9539a0
nikhilgit630 <101937543+nikhilgit630@users.noreply.github.com>
2025-11-07 18:55:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
-/** @type {import('next').NextConfig} */
-
 const isGithub = process.env.NEXT_PUBLIC_IS_GITHUB === "true";
+const forceBase = process.env.NEXT_PUBLIC_FORCE_BASEPATH === "true";
 
-const nextConfig = {
-  output: "export", // ✅ Required for GitHub Pages (Next.js 16)
-  images: {
-    unoptimized: true, // ✅ Needed for static export
-  },
+export default {
+  output: "export",
+  images: { unoptimized: true },
 
-  // ✅ GitHub Pages specific path
-  basePath: isGithub ? "/Portfolio" : "",
-  assetPrefix: isGithub ? "/Portfolio/" : "",
+  // ✅ GitHub Pages basePath force apply
+  basePath: isGithub || forceBase ? "/Portfolio" : "",
+  assetPrefix: isGithub || forceBase ? "/Portfolio/" : "",
 };
-
-export default nextConfig;
